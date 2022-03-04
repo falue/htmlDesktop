27fbb4353f13be9edb167a4fa1ecda9b22fddf12
falue <info@fluescher.ch>
2022-03-04 16:08:49 +0100

    Add video player to Imageviewer

--- programs/imageviewer/imageviewer.js
+++ programs/imageviewer/imageviewer.js
@@ -11,53 +11,86 @@ async function setupImageViewer() {
 
     if(urlParams.get('files')) {
         files = urlParams.get('files').split("|");
-        console.log(files);
-        let content = gebi('content');
         setupThumbnails(files);
-        let path = "../../workstations/"+workstation+"/files/"+files[0];
-        content.style.backgroundImage = "url("+path+")";
+        showImage(0);  // Show first image
     } else {
-        gebi('content').innerHTML = "no files in URL";
+        gebi('content').innerHTML = "No files in URL";
     }
 }
 
 function setupThumbnails(files) {
     let thumbnails = gebi('thumbnails');
+    let content = gebi('content');
     if(files.length <= 1) {
         // Hide navigation arrwos if only one image
-        content.classList.add('onlyOneImage');  
+        content.classList.add('onlyOneImage');
         hide('thumbnails');
     } else {
-        /* FOR OF FILES... */
+        // Setup thumbnail gallery
         for (const [i, file] of files.entries()) {
-        /* for(file of files) { */
-            /* cl(file + " " + i); */
-            /* <div class="thumbnail shadow"></div> */
             let thumbnail = document.createElement("div");
             thumbnail.setAttribute("title", file);
             thumbnail.setAttribute("id", "thumbnail-"+file);
             thumbnail.classList.add("thumbnail", "shadow", "radius3");
-            if(i === 0) thumbnail.classList.add("active");
-            thumbnail.style.backgroundImage = "url(../../workstations/"+workstation+"/files/"+file+")";
+            if(file.endsWith(".mp4")) {
+                thumbnail.style.backgroundImage = "url(../../os/"+os+"/systemIcons/fileMovie.png)";
+                // Preload to allow for scrubbing when visiting the video
+                setVideoSrc("../../workstations/"+workstation+"/files/"+file, 'video/mp4');
+            } else {
+                thumbnail.style.backgroundImage = "url(../../workstations/"+workstation+"/files/"+file+")";
+            }
             thumbnail.setAttribute("onclick", "showImage("+i+");");
             thumbnails.appendChild(thumbnail);
-            /* console.log(i, file) */
         }
     }
 }
 
 function showImage(index) {
-    let path = "../../workstations/"+workstation+"/files/"+files[index];
-    content.style.backgroundImage = "url("+path+")";
+    imageIndex = index;
+    let file = files[index];
+    let path = "../../workstations/"+workstation+"/files/"+file;
+    let content = gebi('content');
+
+    if(file.endsWith(".mp4")) {
+        show("videoPlayer");
+        setVideoSrc(path, 'video/mp4');
+        content.style.backgroundImage = "none";
+    } else {
+        hide("videoPlayer");
+        let player = videojs(document.querySelector('.video-js'));
+        if(player) player.pause();
+        content.style.backgroundImage = "url("+path+")";
+    }
+
     /* Remove all active classes */
     let elements = document.querySelectorAll(".thumbnail.active");
     for(i=0; i< elements.length; i++) {
-          elements[i].classList.remove('active');
-      }
+        elements[i].classList.remove('active');
+    }
+
     /* Scroll to thumbnail & mark as active */
-    let thumbnail = gebi("thumbnail-"+files[index]);
-    thumbnail.scrollIntoView({inline: "center", block: "nearest", behavior: "smooth"});
-    thumbnail.classList.add('active');
+    let thumbnail = gebi("thumbnail-"+file);
+    if(thumbnail) {  // If only one file is in URL, there are no thumbnails
+        thumbnail.scrollIntoView({inline: "center", block: "nearest", behavior: "smooth"});
+        thumbnail.classList.add('active');
+    }
+}
+
+async function setVideoSrc(videoSource, type) {
+    cl("laod video");
+    let player = videojs(document.querySelector('.video-js'));
+    player.src({
+        "src": videoSource,
+        "type": type
+    });
+    // Enable autoplay if more than one file
+    // without prior user interaction, autoplay is forbidden
+    // Fails if video is first of gallery but yeah, just more error logs
+    return await player.ready(function(){
+            if(files.length > 1) {
+                player.play();
+            }
+        });
 }
 
 function navigateGallery(direction) {
