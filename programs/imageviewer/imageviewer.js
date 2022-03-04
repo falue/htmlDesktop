bfbefa651df0a481ea88862bd66aacd8764c4c59
falue <info@fluescher.ch>
2022-03-04 12:56:10 +0100

    Add thumbnail gallery to image viewer

--- programs/imageviewer/imageviewer.js
+++ programs/imageviewer/imageviewer.js
@@ -13,6 +13,7 @@ async function setupImageViewer() {
         files = urlParams.get('files').split("|");
         console.log(files);
         let content = gebi('content');
+        setupThumbnails(files);
         let path = "../../workstations/"+workstation+"/files/"+files[0];
         content.style.backgroundImage = "url("+path+")";
     } else {
@@ -20,9 +21,43 @@ async function setupImageViewer() {
     }
 }
 
+function setupThumbnails(files) {
+    let thumbnails = gebi('thumbnails');
+    if(files.length <= 1) {
+        // Hide navigation arrwos if only one image
+        content.classList.add('onlyOneImage');  
+        hide('thumbnails');
+    } else {
+        /* FOR OF FILES... */
+        for (const [i, file] of files.entries()) {
+        /* for(file of files) { */
+            /* cl(file + " " + i); */
+            /* <div class="thumbnail shadow"></div> */
+            let thumbnail = document.createElement("div");
+            thumbnail.setAttribute("title", file);
+            thumbnail.setAttribute("id", "thumbnail-"+file);
+            thumbnail.classList.add("thumbnail", "shadow", "radius3");
+            if(i === 0) thumbnail.classList.add("active");
+            thumbnail.style.backgroundImage = "url(../../workstations/"+workstation+"/files/"+file+")";
+            thumbnail.setAttribute("onclick", "showImage("+i+");");
+            thumbnails.appendChild(thumbnail);
+            /* console.log(i, file) */
+        }
+    }
+}
+
 function showImage(index) {
     let path = "../../workstations/"+workstation+"/files/"+files[index];
     content.style.backgroundImage = "url("+path+")";
+    /* Remove all active classes */
+    let elements = document.querySelectorAll(".thumbnail.active");
+    for(i=0; i< elements.length; i++) {
+          elements[i].classList.remove('active');
+      }
+    /* Scroll to thumbnail & mark as active */
+    let thumbnail = gebi("thumbnail-"+files[index]);
+    thumbnail.scrollIntoView({inline: "center", block: "nearest", behavior: "smooth"});
+    thumbnail.classList.add('active');
 }
 
 function navigateGallery(direction) {
