04ad53cadd4d965af7bc206fde7e1a76b61bb5ca
falue <info@fluescher.ch>
2022-03-06 20:49:24 +0100

    Texteditor: change fontsize

--- programs/texteditor/texteditor.js
+++ programs/texteditor/texteditor.js
@@ -9,3 +9,21 @@ async function setup() {
 
     textarea.focus();
 }
+
+let fontSizeIndex = 3;
+function changeFontSize() {
+    let fontSizes = [.6, .8, 1, 1.2, 1.5, 1.8, 2.5];
+    cl(fontSizeIndex + " " + fontSizes[fontSizeIndex]);
+    gebi('textarea').style.fontSize = fontSizes[fontSizeIndex] + "em";
+    if(fontSizeIndex < fontSizes.length-1) {
+        fontSizeIndex++
+    } else {
+        fontSizeIndex = 0;
+    }
+}
+
+let randomTexts = [
+    "text1",
+    "text2",
+    "text3",
+]
