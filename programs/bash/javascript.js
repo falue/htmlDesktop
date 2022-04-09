1df890e919846717a3b994855fcb82e845ef8c0b
falue <info@fluescher.ch>
2022-04-09 20:03:02 +0200

    Add freeText command with basic bash functions; move stuff around

--- programs/bash/javascript.js
+++ programs/bash/javascript.js
@@ -2,6 +2,7 @@ let bashProfileName;
 let commands = [];
 let commandIndex = -1;
 let blockCommand = false;
+let freeTextCommand = "";
 
 async function setup() {
   const queryString = window.location.search;
@@ -9,33 +10,58 @@ async function setup() {
   let os = urlParams.get("os");
   let workstation = urlParams.get("workstation");
   let darkMode = urlParams.get("darkMode");
-  let scriptName = urlParams.get("script") || "script";
+  let scriptName = urlParams.get("script");
+  let script = [];
+  if(scriptName) {
+    script = await parseFile(
+      `../../workstations/${workstation}/bash/${scriptName}.fakeBash`
+    );
+  } else {
+    // make default bashProfile and enable freetext bashing
+    script[0] = `<span class='green'>admin</span>@<span class='red'>localhost</span>:<span class='path grey'>~</span> $ `;
+    script.push(`[bashProfile] [freeText]`);
+    script.push(`[goto:nobr,0]`);
+    darkMode = "true";
+  }
 
   if(darkMode === "true") {
     addStylesheet("darkMode.css");
   }
 
-  let script = await parseFile(
-    `../../workstations/${workstation}/bash/${scriptName}.fakeBash`
-  );
-  loadScript(script);
+  commands = loadScript(script);
   playCommandAtIndex(); // Play first command
 }
 
-function loadScript(script) {
-  bashProfileName = script.shift(); // Get first element and delete it
+/* GENERAL */
+
+function addStylesheet(path) {
+  let currentStylesheet = gebi('osStylesheet');
+  if(currentStylesheet) currentStylesheet.remove();
+  let head = document.head;
+  let link = document.createElement("link");
+  link.id = "osStylesheet"
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = path;
+  head.appendChild(link);
+}
+
+function loadScript(script, getBashProfile = true) {
+  let localCommands = [];
+  if(getBashProfile) bashProfileName = script.shift(); // Get first element and delete it
   for (lines of script) {
     if (lines === "EXIT") break; // Abort parsing file when keyword EXIT is found
-    splitCommandLines(lines);
+    localCommands = [...localCommands, ...splitCommandLines(lines)];
     if (lines != "[br]" && !lines.includes(":nobr")) {
       // Do not add br for "br" "nobr" command
-      commands.push({ function: "br" });
+      localCommands.push({ function: "br" });
     }
   }
-  // cl(commands);
+  return localCommands;
 }
 
 function splitCommandLines(line) {
+  let localCommands = [];
   // Split multiple commands "[command1] [command2]" on one line into multiple functions
   // let reBrackets = /\[(.*?)\]/g;  // finds also [ and ] in quotes
   let reBrackets = /(?<!\\)\[(.*?)(?<!\\)\]/g; // ingores \[ and \]
@@ -56,12 +82,13 @@ function splitCommandLines(line) {
             .replace(/\\\]/g, "]")
         : parseInt(element);
     });
-    commands.push({
+    localCommands.push({
       function: nameOfFunction,
       parameters: keys.length ? [...keys] : "",
       classes,
     });
   }
+  return localCommands;
 }
 
 function printToConsole(text, classes) {
@@ -90,6 +117,33 @@ function playCommandAtIndex(index) {
     commandIndex = index;
   }
 }
+
+function resetConsole() {
+  gebi("console").innerHTML = "";
+  playCommandAtIndex(0);
+}
+
+function removeLastCommandOutput(className) {
+  // If no class is specified, remove every command
+  let lastCommandOutput = document.querySelectorAll(`#console${className ? ` .${className}` : '>*'}`);
+  if (lastCommandOutput[lastCommandOutput.length-1]) {
+    lastCommandOutput[lastCommandOutput.length-1].remove();
+  }
+}
+
+function waitForKey(keyCode) {
+  return new Promise((resolve) => {
+    document.addEventListener("keydown", onKeyHandler);
+    function onKeyHandler(e) {
+      if (e.keyCode === keyCode) {
+        document.removeEventListener("keydown", onKeyHandler);
+        resolve();
+      }
+    }
+  });
+}
+
+/* SPECIFIC */
 
 function setupForceType(command) {
   let uid = createUniqueId();
@@ -191,22 +245,85 @@ async function counterAscii(
   return;
 }
 
-function resetConsole() {
-  gebi("console").innerHTML = "";
-  playCommandAtIndex(0);
-}
-
-function waitForKey(keyCode) {
+function freeText(keyCode) {
   return new Promise((resolve) => {
     document.addEventListener("keydown", onKeyHandler);
-    function onKeyHandler(e) {
+    async function onKeyHandler(e) {
       if (e.keyCode === keyCode) {
         document.removeEventListener("keydown", onKeyHandler);
+        evalCommand(freeTextCommand);
+        freeTextCommand = "";
         resolve();
+      } else {
+        // Ignore shift, ctrl, alt, meta, etc.
+        let printCommand = "";
+        if (e.key.length === 1) {
+          printCommand = e.key;
+        } else if (e.keyCode === 8) {
+          removeLastCommandOutput("freeText");
+          freeTextCommand = freeTextCommand.slice(0, -1);
+          return;
+        } else if (e.keyCode === 78) {
+          printCommand = "~";
+        } else if (e.keyCode === 187 && e.shiftKey) {
+          printCommand = "^";
+        } else if (e.keyCode === 187 && !e.shiftKey) {
+          printCommand = "`";
+        } else if (e.keyCode === 221) {
+          printCommand = "¨";
+        }
+
+        freeTextCommand += printCommand;
+        printToConsole(printCommand, "freeText");
       }
     }
   });
 }
+
+async function evalCommand(command) {
+  command = command.replace("sudo ", "").replace("sudo", "");
+  // For 'cd' to work, initial bashProfileName (path) must include "..~<.." !
+  if(command === "cd") {
+    return;
+  } else if(command === "cd ~") {
+    bashProfileName = `${bashProfileName.replace(/\~(.*?)\</, `~<`)}`;
+  } else if(command.startsWith("cd ..")) {
+    bashProfileName = `${bashProfileName.replace(/\~(.*?)\/[^\/]+\</, `~$1<`)}`;
+  } else if(command.startsWith("cd .")) {
+    return;
+  } else if(command.startsWith("cd ")) {
+    // Replace path with current command parameter
+    bashProfileName = `${bashProfileName.replace(/(\~.*?)\</, `$1/${command.replace("cd ", "")}<`)}`;
+  } else if(command === "ls") {
+    printToConsole(`<br>bin   dev  home  lost+found  mnt  proc  run   srv  tmp  var<br>
+  boot  etc  lib   media       opt  root  sbin  sys  usr`, "grey");
+  } else if(command.startsWith("ls -l")) {
+    printToConsole(`<br>total 60<br>
+lrwxrwxrwx   1 root root     7 Jan 11  2021 bin -> usr/bin<br>
+drwxr-xr-x   5 root root  4096 Jan  1  1970 boot<br>
+drwxr-xr-x  18 root root  3820 Feb 24 09:17 dev<br>
+drwxr-xr-x 118 root root  4096 Feb 21  2021 etc<br>
+drwxr-xr-x   3 root root  4096 Jan 11  2021 home<br>
+lrwxrwxrwx   1 root root     7 Jan 11  2021 lib -> usr/lib<br>
+drwx------   2 root root 16384 Jan 11  2021 lost+found<br>
+drwxr-xr-x   2 root root  4096 Jan 11  2021 media<br>
+drwxr-xr-x   3 root root  4096 Jan 27  2021 mnt<br>
+drwxr-xr-x   4 root root  4096 Jan 11  2021 opt<br>
+dr-xr-xr-x 137 root root     0 Jan  1  1970 proc<br>
+drwx------   5 root root  4096 Jan 27  2021 root<br>
+drwxr-xr-x  29 root root   860 Apr  9 15:46 run<br>
+lrwxrwxrwx   1 root root     8 Jan 11  2021 sbin -> usr/sbin<br>
+drwxr-xr-x   2 root root  4096 Jan 11  2021 srv<br>
+dr-xr-xr-x  12 root root     0 Jan  1  1970 sys<br>
+drwxrwxrwt  10 root root  4096 Apr  9 12:53 tmp<br>
+drwxr-xr-x  11 root root  4096 Jan 11  2021 usr<br>
+drwxr-xr-x  11 root root  4096 Jan 11  2021 var`, "grey");    
+  } else if(command) {
+    printToConsole(`<br>-bash: ${command}: command not found`, "grey");
+  }
+}
+
+/* PLAY / REPLAY / KEYBOARD INPUT EVALUATION */
 
 async function playCommand(command) {
   // Execute command
@@ -225,6 +342,11 @@ async function playCommand(command) {
     case "pwd":
       show("cursor");
       await waitForKey(13);
+      break;
+    case "freeText":
+      show("cursor");
+      await freeText(13);
+      playCommandAtIndex();
       break;
     case "sleep":
       await delay(command.parameters[0]);
@@ -249,23 +371,11 @@ async function playCommand(command) {
   body.scrollTop = body.scrollHeight;
 
   // If not waiting for the user, play next command automatically
-  if (["forceType", "wait"].includes(command.function)) {
+  if (["forceType", "wait", "freeText"].includes(command.function)) {
     // Halt for user input
     show("cursor");
   } else {
     hide("cursor");
     if (!["nobr"].includes(command.classes)) playCommandAtIndex();
   }
-}
-
-function addStylesheet(path) {
-  let currentStylesheet = gebi('osStylesheet');
-  if(currentStylesheet) currentStylesheet.remove();
-  let head = document.head;
-  let link = document.createElement("link");
-  link.id = "osStylesheet"
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.href = path;
-  head.appendChild(link);
 }
