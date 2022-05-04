4123369094bd437855dd43bff9fe7d8fe0cbc151
falue <info@fluescher.ch>
2022-05-04 23:21:55 +0200

    auto generate textblocks from js files

---
+++ programs/neptcode/data/1.js
@@ -0,0 +1,6 @@
+let data = [
+    "type some python & press enter!",
+    "type some more",
+];
+
+let out = "Final agreement.";
--- programs/neptcode/javascript.js
+++ programs/neptcode/javascript.js
@@ -1,3 +1,5 @@
+let codeIndex;
+
 async function setup() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -5,6 +7,7 @@ async function setup() {
     let workstation = urlParams.get('workstation');
     let darkMode = urlParams.get('darkMode');
     let scene = urlParams.get('scene');
+    let dataPath = urlParams.get('data');
     
     // Set generic system fonts
     setSystemFont(os);
@@ -12,10 +15,8 @@ async function setup() {
     if(darkMode === "true") {
         addStylesheet("darkMode.css", false);
         addStylesheet("../../tools/libraries/highlight/styles/monokai-sublime.css", false);
-        /* addStylesheet("../../tools/libraries/highlight/styles/tokio-night-dark.css", false); */
     } else {
         addStylesheet("../../tools/libraries/highlight/styles/docco.css", false);
-        /* addStylesheet("lightMode.css"); */
     }
 
     // select all textareas and set onkeyup to setTextareaHeightToEnteredText
@@ -33,6 +34,14 @@ async function setup() {
 
     // Open the start tab
     switchTabs(scene || 1);
+
+    // Setup code blocks
+    // Add new script tag with src to head
+    let script = document.createElement('script');
+    script.src = `data/${dataPath || "basic"}.js`;
+    // Wait for additional js to load until commencing setup process
+    script.setAttribute('onload', 'setupCodeblocks()');
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
 
 
@@ -51,9 +60,11 @@ function updateSyntaxHighlighting() {
     });
 }
 
-function forceTypeHighlight(event, textarea, target, text) {
+function forceTypeHighlight(event, textarea, target, text, endAction, waitForEnter) {
     cursor(target, false);
-    forceType(event, textarea, text, false, true);
+    forceType(event, textarea, text, endAction, waitForEnter);
+    cl(text);
+    cl(textarea.value);
     gebi(target).innerHTML=textarea.value.replaceAll('\n', '<br>').replaceAll('`', "'").replaceAll('’', "'");
     updateSyntaxHighlighting();
     // Do not show cursor if text is finished
@@ -74,18 +85,8 @@ function cursor(terminalId, display) {
 }
 
 function clearInputs() {
-    let inputs = document.getElementsByTagName('input');
-    let textareas = document.getElementsByTagName('textarea');
-    let highlights = document.getElementsByClassName('terminal');
-    for (let i = 0; i < inputs.length; i++) {
-        inputs[i].value = '';
-    }
-    for (let i = 0; i < textareas.length; i++) {
-        textareas[i].value = '';
-    }
-    for (let i = 0; i < highlights.length; i++) {
-        highlights[i].innerHTML = '';
-    }
+    gebi("paper").innerHTML = '';
+    setupCodeblocks();
 }
 
 function setTextareaHeightToEnteredText() {
@@ -95,6 +96,75 @@ function setTextareaHeightToEnteredText() {
         textareas[i].style.height = textareas[i].scrollHeight + 'px';
     }
 }
+
+function setupCodeblocks() {
+    codeIndex = 0;
+    addNextCodeblock();
+    updateSyntaxHighlighting();
+}
+
+/* AUTO CREATE CODE */
+async function addNextCodeblock(last=false) {
+    // If end is reached, get on to print out the final message
+    if(codeIndex >= data.length && !last) {
+        addNextCodeblock(true);
+        return;
+    }
+
+    let container = gebi("paper");
+    let uniqueId = createUniqueId();
+    let label = document.createElement("label");
+    let labelText = document.createElement("div");
+    labelText.appendChild(document.createTextNode(last ? `Out [${codeIndex-1}]:` : `In [${codeIndex}]:`));
+    label.appendChild(labelText);
+    let forcetypeContainer = document.createElement("div");
+    forcetypeContainer.setAttribute("class", "forceType");
+    let textarea = document.createElement("textarea");
+    textarea.setAttribute("onblur", `cursor('terminal-${uniqueId}', false)`);
+    if(!last) {
+        textarea.setAttribute("onfocus", `cursor('terminal-${uniqueId}', true)`);
+        textarea.setAttribute("onkeydown", `forceTypeHighlight(event, this, 'terminal-${uniqueId}', '${data[codeIndex].replaceAll('\n' ,'<br>')}', function () { addNextCodeblock() }, true);`);
+    }
+    forcetypeContainer.appendChild(textarea);
+    let highlightBox = document.createElement("div");
+    highlightBox.setAttribute("id", `terminal-${uniqueId}`);
+    highlightBox.setAttribute("class", "terminal python");
+    forcetypeContainer.appendChild(highlightBox);
+    label.appendChild(forcetypeContainer);
+    container.appendChild(label);
+    
+    if(last) {
+        // print out the final message
+        document.activeElement.blur();
+        highlightBox.innerHTML = "<i class='material-icons small valign blue spin'>sync</i>";
+        highlightBox.classList.add('op50');
+        await delay(1111);
+        highlightBox.classList.remove('op50');
+        if(out.match("<")) {
+            // If its html, remove highlighting and add it as is
+            highlightBox.removeAttribute("class");
+            highlightBox.innerHTML = out;
+            highlightBox.classList.add('out');
+        } else {
+            highlightBox.innerHTML = out;
+            textarea.focus();
+            updateSyntaxHighlighting();
+        }
+        codeIndex = 0;
+        await delay(100);  // wait for image to load so scroll can happen
+        scrollToBottom("paperHolder");
+    } else {
+        // focus() this element
+        focusNextElement();
+        codeIndex++;
+    }
+}
+
+function scrollToBottom(id){
+    let container = document.getElementById(id);
+    container.scrollTop = container.scrollHeight - container.clientHeight;
+}
+
 
 function addStylesheet(path, replace=true) {
     let head = document.head;
