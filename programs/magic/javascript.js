831060b2e818261b4bc0aa946ae4afcf15964c4f
falue <info@fluescher.ch>
2022-05-19 16:19:42 +0200

    Add program Magic; allow window icons to be changed

---
+++ programs/magic/javascript.js
@@ -0,0 +1,18 @@
+async function setup() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let os = urlParams.get('os');
+    /* let workstation = urlParams.get('workstation');
+    let darkMode = urlParams.get('darkMode'); */
+    /* let id = urlParams.get('id'); */
+    /* let id = window.parent.document.getElementById('#target');  */
+    let thisWindow = window.parent.document.querySelectorAll("[data-setup*='magic']")[0];
+    let id = thisWindow.id;
+    let newSrc = await parent.showDialog("Set new window path", "Type new absolute path or URL.<br>Including localhost or live URLs.", false, "https://");
+    if(newSrc && newSrc != "~EMPTY") {
+        let windowTitle = await parent.setWindowTitle(`title-${id}`);
+        let windowIcon = await parent.setWindowIcon(`${id}`);
+        thisWindow.setAttribute('data-setup', `['${windowTitle}', '${windowIcon}', '${newSrc}']`);
+        thisWindow.getElementsByTagName('iframe')[0].src = newSrc;
+    }
+}
