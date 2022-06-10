4a3115f0e2ae3c8574a76e8d40514af1811e8187
falue <info@fluescher.ch>
2022-06-10 16:17:49 +0200

    Next try in hardReloading iFrame

--- tools/windowmanager.js
+++ tools/windowmanager.js
@@ -503,11 +503,12 @@ async function hardReloadAllIframes() {
   let iFrames = document.querySelectorAll("iframe");
   for (let currentIframe of iFrames) {
     let currentSrc = currentIframe.src;
-    currentIframe.src = '';
+    currentIframe.setAttribute('src', 'about:blank');
     await delay(666);
+    cl(`Hard reloading: ${currentSrc}`);
     // Add random integer to URL for force reloading. does that every time. meh.
     let rand = Math.floor((Math.random()*1000000)+1);
-    currentIframe.src = `${currentSrc}${currentSrc.includes('?') ? "&" : "?" }forceReload=${rand}`;
+    currentIframe.setAttribute('src', `${currentSrc}${currentSrc.includes('?') ? "&" : "?" }forceReload=${rand}`);
   }
 }
 
