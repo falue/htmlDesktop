713375e4f1c2e6924431c4640ba764336f8b5ede
falue <info@fluescher.ch>
2024-01-16 17:33:53 +0100

    Add hacking login credentials

--- tools/windowmanager.js
+++ tools/windowmanager.js
@@ -355,6 +355,8 @@ async function addWindow(windowName, icon, contentPath, x,y, w,h, minimized, zIn
 
   makeResizableDiv('#'+id);
   setSystemColors(systemColor);
+
+  return id;
 }
 
 function getFrontMostWindow(includingMinimized) {
