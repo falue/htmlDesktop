b70b484eee40e60fb524764980fbfaf2762bf173
falue <info@fluescher.ch>
2024-01-16 17:31:42 +0100

    add print window

---
+++ programs/print/javascript.js
@@ -0,0 +1,40 @@
+async function setup() {
+    /* 
+        URL PARAMS:
+        - set "printText" to a text/encoded html string to be displayed as plain text
+        - set "printIframe" to eg. programs/wifi to display an program in the print area
+        - set "pages" to set "to" pages
+    */
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let os = urlParams.get('os');
+    let workstation = urlParams.get('workstation');
+    let darkMode = urlParams.get('darkMode');
+    
+    let pages = urlParams.get('pages');
+    if(pages) {
+        gebi('pageIndicator1').innerHTML = pages;
+        gebi('range_to').value = pages;
+    }
+
+    let printText = urlParams.get('printText');
+    let printIframe = urlParams.get('printIframe');
+    if(printText || printIframe) {
+        show('preview');
+        gebi('form').style.width="60%";
+        gebi('form').style.left="40%";
+        if(printText) {
+            gebi('paper').innerHTML = printText;
+        } else {
+            cl(" printIframe")
+            cl( printIframe)
+            gebi('paper').style.padding="0";
+            gebi('paper').innerHTML = `<iframe src="${ printIframe}"></iframe>`;
+        }
+    } else {
+        hide('preview');
+    }
+    
+    // Set generic system fonts
+    setSystemFont(os);
+}
