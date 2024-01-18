549f5b5dd4ef3cabc9c4a571aaa5f39fa4734c92
falue <info@fluescher.ch>
2024-01-18 03:27:50 +0100

    Add blue screen of death

---
+++ programs/bluescreen/javascript.js
@@ -0,0 +1,9 @@
+async function setup() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let os = urlParams.get('os');
+    
+    // title
+    // text
+    // cursor
+    }
