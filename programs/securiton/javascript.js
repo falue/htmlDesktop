f08aa9408bdf3a51aca2315b106abff01456ec61
falue <info@fluescher.ch>
2024-02-18 10:57:09 +0100

    Add videos for 618 to charles; 1x1 video in securiton possible

--- programs/securiton/javascript.js
+++ programs/securiton/javascript.js
@@ -1,4 +1,12 @@
 let scene;
+let videoIndex = 1;
+let totalVideos = 4;
+let useGreenscreen = false;
+let showControls = false;
+let greenscreenType = 1;
+let gridSize = 4;
+let zoomStage = [1,1,1,1,1];
+
 async function setup() {
     /* 
         URL parameters
@@ -19,6 +27,16 @@ async function setup() {
         showVideo();
         gebi('video4').pause();
         videoIndex = 1;
+        if(localStorage.getItem('securiton-gridsize1') === "true") {
+            //showVideo();
+            setGrid(1);
+        }
+
+        if(localStorage.getItem('securiton-hideControls') === "true") {
+            setControls(false);
+        } else {
+            setControls(true);
+        }
     }
         
     /* let form = gebi('loginForm');
@@ -45,12 +63,6 @@ async function setup() {
     } */
 }
 
-
-let videoIndex = 1;
-let totalVideos = 4;
-let useGreenscreen = false;
-let greenscreenType = 1;
-
 function setGreenscreenFlag(mode) {
     useGreenscreen = mode;
     if(mode) {
@@ -69,6 +81,9 @@ function setGreenscreenFlag(mode) {
 } */
 
 function showVideo() {
+
+    cl(zoomStage)
+
     if(videoIndex > totalVideos) {
         // Reset videos
         videoIndex=1;
@@ -76,14 +91,17 @@ function showVideo() {
         let videoElem1 = gebi('video2');
         let videoElem2 = gebi('video3');
         let videoElem3 = gebi('video4');
+        let videoElem4 = gebi('video5');
         videoElem0.setAttribute('src', 'data/default.mp4');
         videoElem1.setAttribute('src', 'data/default.mp4');
         videoElem2.setAttribute('src', 'data/default.mp4');
         videoElem3.setAttribute('src', 'data/default.mp4');
+        videoElem4.setAttribute('src', 'data/default.mp4');
         videoElem0.pause();
         videoElem1.pause();
         videoElem2.pause();
         videoElem3.pause();
+        videoElem4.pause();
         videoElem0.style.transform = 'scale(1)';
         videoElem0.style.transformOrigin = '0px 0px';
         videoElem1.style.transform = 'scale(1)';
@@ -92,9 +110,11 @@ function showVideo() {
         videoElem2.style.transformOrigin = '0px 0px';
         videoElem3.style.transform = 'scale(1)';
         videoElem3.style.transformOrigin = '0px 0px';
-        zoomStage = [1,1,1,1]
+        videoElem4.style.transform = 'scale(1)';
+        videoElem4.style.transformOrigin = '0px 0px';
+        zoomStage = [1,1,1,1,1]
         if(useGreenscreen) toggleGreenscreen(useGreenscreen);
-        return;
+        //return;
     }
 
     // let videoName = useGreenscreen ? 'data/greenscreen'+greenscreenType+'.mp4' : 'data/'+videoIndex+'.mp4';
@@ -108,8 +128,12 @@ function showVideo() {
     gebi('video2').pause();
     gebi('video3').pause();
     gebi('video4').pause();
+    gebi('video5').pause();
     if(!useGreenscreen) gebi('video'+videoIndex).play();
 
+    if(gridSize === 1) {
+        setVideoSizes(videoIndex, 100);
+    }
     videoIndex++;  
 }
 
@@ -119,27 +143,31 @@ function toggleGreenscreen(mode) {
         hide('video2');
         hide('video3');
         hide('video4');
+        hide('video5');
         show('greenscreen1');
         show('greenscreen2');
         show('greenscreen3');
         show('greenscreen4');
+        show('greenscreen5');
         gebi('video1').pause();
         gebi('video2').pause();
         gebi('video3').pause();
         gebi('video4').pause();
+        gebi('video5').pause();
     } else {
         show('video1');
         show('video2');
         show('video3');
         show('video4');
+        show('video5');
         hide('greenscreen1');
         hide('greenscreen2');
         hide('greenscreen3');
         hide('greenscreen4');
+        hide('greenscreen5');
     }
 }
 
-let zoomStage = [1,1,1,1];
 
 function zoom(event, elementNumber) {
     /* console.log(event.offsetX, event.offsetY); */
@@ -166,7 +194,7 @@ function zoom(event, elementNumber) {
         
         // if zoom is maxed out, reset controls
         if(zoomStage[elementNumber-1] == 1) {
-            videoElem.setAttribute('controls', '');
+            if(showControls) videoElem.setAttribute('controls', '');
         }
     }
 }
@@ -184,5 +212,102 @@ function togglePlay(videoIndex) {
             cl('toggle pause')
             videoElem.pause();
         }
+    }
+}
+
+function setGrid(size) {
+    if(size === 1) {
+        totalVideos = 5;
+        gridSize = size;
+        localStorage.setItem('securiton-gridsize1', 'true');
+        gebi('gridBtn1').classList.add('greyBg');
+        gebi('gridBtn4').classList.remove('greyBg');
+
+        setVideoSizes(1, 100);
+
+    } else {
+        gridSize = size;
+        totalVideos = 4;
+        localStorage.removeItem('securiton-gridsize1');
+        gebi('gridBtn1').classList.remove('greyBg');
+        gebi('gridBtn4').classList.add('greyBg');
+
+        setVideoSizes(1, 50);
+        setVideoSizes(2, 50);
+        setVideoSizes(3, 50);
+        setVideoSizes(4, 50);
+    }
+}
+
+function setVideoSizes(index, size) {
+    if(size===100) {
+        hide('container1');
+        hide('container2');
+        hide('container3');
+        hide('container4');
+        hide('container5');
+        show('container'+index);
+        gebi('container'+index).style.left = '0';
+        gebi('container'+index).style.top = '0';
+    } else {
+        show('container1');
+        show('container2');
+        show('container3');
+        show('container4');
+        hide('container5');
+
+        switch(index) {
+            case 1:
+                gebi('container'+index).style.left = '0';
+                gebi('container'+index).style.top = '0';
+                break;
+            
+            case 2:
+                gebi('container'+index).style.left = '50%';
+                gebi('container'+index).style.top = '0';
+                break;
+
+            case 3:
+                gebi('container'+index).style.left = '0';
+                gebi('container'+index).style.top = '50%';
+                break;
+
+            case 4:
+                gebi('container'+index).style.left = '50%';
+                gebi('container'+index).style.top = '50%';
+                break;
+        }
+    }
+    
+    gebi('container'+index).style.height = size+"%";
+    gebi('container'+index).style.width = size+"%";
+}
+
+function setControls(mode) {
+    if(mode) {
+        showControls = true;
+        localStorage.setItem('securiton-hideControls', false);
+        
+        gebi('controlsNo').classList.remove('greyBg');
+        gebi('controlsYes').classList.add('greyBg');
+
+        gebi('video1').setAttribute("controls","controls");
+        gebi('video2').setAttribute("controls","controls");
+        gebi('video3').setAttribute("controls","controls");
+        gebi('video4').setAttribute("controls","controls");
+        gebi('video4').setAttribute("controls","controls");
+
+    } else {
+        showControls = false;
+        localStorage.setItem('securiton-hideControls', true);
+
+        gebi('controlsNo').classList.add('greyBg');
+        gebi('controlsYes').classList.remove('greyBg');
+
+        gebi('video1').removeAttribute("controls");
+        gebi('video2').removeAttribute("controls");
+        gebi('video3').removeAttribute("controls");
+        gebi('video4').removeAttribute("controls");
+        gebi('video4').removeAttribute("controls");
     }
 }
