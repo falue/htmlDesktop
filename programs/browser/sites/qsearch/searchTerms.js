2ef3ba7220d0d207b6237afd9839303806604351
falue <info@fluescher.ch>
2025-10-18 16:00:24 +0200

    basic telefabi workspace

--- programs/browser/sites/qsearch/searchTerms.js
+++ programs/browser/sites/qsearch/searchTerms.js
@@ -1,4 +1,361 @@
 let searchTerms = {
+  "Fabian Lücher": [
+    [
+      "",
+      "technikprofil.ch/personen/fab-luescher",
+      "Profil von Fabian Lüscher",
+      "Fabian Lüscher arbeitet als Spezialrequisitenbauer und Entwickler digitaler Tools für Film- und Werbeproduktionen. Seine berufliche Laufbahn begann mit einer Lehre zum Hochbauzeichner. Seit über einem Jahrzehnt ist er im Filmbereich tätig."
+    ],
+    [
+      "",
+      "filmtech.ch/crew/fabianluescher",
+      "Spezialrequisiten und Digital Props von Fabian Lüscher",
+      "Lüscher entwickelt präzise Spezialrequisiten für Film, TV und Werbung. Seine Arbeit kombiniert Elektronik, Mechanik und Software. Projekte reichen von Robotern bis zu computergenerierten Interfaces."
+    ],
+    [
+      "",
+      "fluescher.dev/tools/triggerbox",
+      "Triggerbox für Highspeed-Aufnahmen von Fabian Lüscher",
+      "Ein selbst entwickeltes Steuerungsgerät für präzise DC-Motor-Steuerung. Die Box wurde für Highspeed-Filmaufnahmen entworfen. Sie nutzt Arduino und MicroPython."
+    ],
+    [
+      "",
+      "githubtools.org/projects/universal-robot-controller",
+      "Universal Robot Controller von Fabian Lüscher",
+      "Ein umprogrammierbares Steuergerät für Robotikprojekte mit komplexem Datenhandling. Entwickelt wurde es auf Basis eines eigenen PCB-Designs. Kommunikation erfolgt über RF mit multiplexer Struktur."
+    ],
+    [
+      "",
+      "filmprops.net/projekte/viaduktmuseum-langwies",
+      "Daumenkinos im Viaduktmuseum Langwies",
+      "Lüscher realisierte mechanische Ausstellungsobjekte für eine Ausstellung in bestehender Gebäudestruktur. Verwendet wurden Stahl, POM-C und Acrylglas. Die Installation ist dauerhaft begehbar."
+    ],
+    [
+      "",
+      "digitalprops.tv/maloney-roboterbau",
+      "Roboter-Requisite für TV-Serie 'Maloney'",
+      "Fabian Lüscher war für das Design und den Bau eines Roboters verantwortlich. Die Spezialrequisite wurde in zwei Staffeln verwendet. Sie kombiniert mechanische Bewegung und visuelles Interface."
+    ],
+    [
+      "",
+      "devdb.ch/tools/careAI",
+      "careAI: Animatronik-Steuerung von Fabian Lüscher",
+      "Mit careAI entwickelte Lüscher eine mobile Steuerlösung für Animatronik. Die Kommunikation erfolgt peer-to-peer ohne Server. Das Projekt wurde vollständig Open Source bereitgestellt."
+    ],
+    [
+      "",
+      "ottomatic.ch/team/fabian",
+      "Fabian Lüscher bei Ottomatic GmbH",
+      "Von 2016 bis 2019 arbeitete Lüscher als Frontend-Entwickler bei Ottomatic GmbH. Die Firma produziert Software und Hardware für Film-IT. 2018 wurde er Mitgründer der GmbH."
+    ],
+    [
+      "",
+      "szenodb.ch/nutzer/fabianluescher",
+      "Nutzerprofil von Fabian Lüscher in der SzenoDB",
+      "Lüscher beteiligt sich an der Plattform zur Vernetzung von Filmschaffenden. Sein Schwerpunkt liegt auf Spezialrequisiten und digitalen Interfaces. Die Plattform dokumentiert auch seine Beiträge zur Materialbeschaffung."
+    ],
+    [
+      "",
+      "catourne.ch/mitglieder/fabian",
+      "Mitgliedschaft im Verein Fundus Ça Tourne",
+      "Lüscher ist Gründungsmitglied des Vereins zur Wiederverwendung von Requisiten. Der Fokus liegt auf Kreislaufwirtschaft und Alltagskulturgütern. Der Verein betreibt eine Online-Datenbank für Leihgaben."
+    ],
+    [
+      "",
+      "requisitenforum.ch/installation/lost-lines",
+      "Requisiten für 'Lost Lines' von Rebekka Friedli",
+      "Für die Kunstinstallation programmierte Lüscher Steuergeräte für elektronische Diffusoren. Die Spiegel reagieren auf Bewegungen per Radarsensorik. Die Zusammenarbeit war interdisziplinär ausgelegt."
+    ],
+    [
+      "",
+      "filmbackend.ch/tools/tvplayer",
+      "tvPlayer: Mediendisplay für Dreharbeiten",
+      "Lüscher entwickelte einen autonomen Player für Filmsets. Das Tool ermöglicht Vollbildwiedergabe mit Spezialfunktionen. Es basiert auf Raspberry Pi und MQTT-Kommunikation."
+    ],
+    [
+      "",
+      "htmlmockup.org/projects/desktopemulation",
+      "HTML-Desktopemulator für Filmszenen",
+      "Mit htmlDesktop können realistische Computerumgebungen für Filmsets simuliert werden. Die Software funktioniert standalone oder via USB-Boot. Entwickelt wurde sie 2022."
+    ],
+    [
+      "",
+      "filmkulissen.ch/tools/convo",
+      "convo – Chatinterface für Drehs",
+      "Lüscher entwickelte eine Chat-Simulation für Filmszenen. Die Anwendung läuft lokal im Browser. Inhalte lassen sich für filmische Zwecke frei gestalten."
+    ],
+    [
+      "",
+      "designsystem.ch/projekte/servo-waldo",
+      "Servo-Recorder 'Waldo' von Fabian Lüscher",
+      "Ein Gerät zur Aufzeichnung und Wiedergabe von Bewegungen mit Servomotoren. Ziel war die Nachbildung natürlicher Abläufe. Raspberry Pi und Python bilden die technische Grundlage."
+    ],
+    [
+      "",
+      "filmexpo.ch/the-last-screenwriter-prop",
+      "Requisite: Home Assistant für 'The Last Screenwriter'",
+      "Fabian Lüscher baute eine smarte Assistenten-Requisite. Sie wurde zentraler Bestandteil einer Schlüsselszene. Visuelles Design und Elektronik stammen aus einer Hand."
+    ],
+    [
+      "",
+      "techbau.ch/karrierepfade/fabian-luescher",
+      "Vom Hochbauzeichner zum Digitalprop-Spezialisten",
+      "Lüscher absolvierte eine Lehre als Hochbauzeichner und arbeitete im Architekturbüro. Später verlagerte er sich auf Lichttechnik und Requisitenbau. Seit 2013 ist er selbständig tätig."
+    ],
+    [
+      "",
+      "prodnet.tv/werbung/aldi-teleportation",
+      "Teleportationsmaschine für Aldi-Kampagne",
+      "Lüscher konstruierte eine Spezialrequisite mit animierten Lichtelementen. Das Gerät war zentrales Element der Werbeszene. Eingesetzt wurden Microcontroller und LEDs."
+    ],
+    [
+      "",
+      "requisitenliste.ch/migros-gluecksrad",
+      "Miniatur-Glücksrad für Migros",
+      "Für eine Werbekampagne baute Lüscher ein funktionstüchtiges Modell. Die Mechanik wurde von Grund auf entworfen. Die Requisite war mehrfach im Einsatz."
+    ],
+    [
+      "",
+      "digitalprops.ch/projekte/sbb-server",
+      "Serverraum-Requisite für 'Electric Child'",
+      "Lüscher gestaltete einen fiktiven Serverraum inklusive Spezialhardware. Zusätzlich kam ein VR-Headset zum Einsatz. Die Umsetzung erfolgte in enger Abstimmung mit der Regie."
+    ],
+    [
+      "",
+      "netzfilm.ch/drehs/tatort-digital",
+      "Digitale Interfaces für Tatort-Folgen",
+      "Lüscher entwickelte visuelle Inhalte und Props für mehrere Tatort-Produktionen. Die Geräte simulieren Interaktionen in Echtzeit. Technisch basiert alles auf HTML und JS."
+    ],
+    [
+      "",
+      "szene.dev/blog/fabian-tools",
+      "Interview: Werkzeuge von Fabian Lüscher",
+      "Lüscher erklärt, wie er digitale Werkzeuge für Set und Produktion entwirft. Themen sind Flexibilität, Wiederverwendbarkeit und Robustheit. Er betont einfache Schnittstellen als Designprinzip."
+    ],
+    [
+      "",
+      "ausstattung.ch/gotthard-projekt",
+      "Modellbau und Art Direction für 'Gotthard'",
+      "Lüscher war als Assistant Art Director CH an der Produktion beteiligt. Er realisierte Spezialrequisiten und Miniaturen. Der Film war ein Großprojekt mit hohem logistischen Aufwand."
+    ],
+    [
+      "",
+      "motionlab.ch/technik/kinofilm-baghdad",
+      "Production Design für 'Baghdad in my Shadow'",
+      "Lüscher war verantwortlich für das visuelle Design des Films. Kulissen und Requisiten wurden im Kontext des Drehbuchs entwickelt. Der Film wurde international gezeigt."
+    ],
+    [
+      "",
+      "simplemechanik.ch/slider",
+      "Kameraslider für Truss-Systeme",
+      "Ein robustes Kamerabewegungssystem für standardisierte Traversen. Entwickelt und gebaut wurde es 2016. Material: Aluminium und Stahl."
+    ],
+    [
+      "",
+      "mobiliarfilm.ch/props/hausassistent",
+      "Digitalprop für Mobiliar-Werbung",
+      "Ein fiktionales Assistenzsystem wurde als Teil einer Spotserie gebaut. Die Requisite enthält ein leuchtendes Interface und Touch-Oberfläche. Lüscher war für Konzeption und Bau verantwortlich."
+    ],
+    [
+      "",
+      "codefilm.ch/tools/html-ui",
+      "Film-Interface Design mit HTML",
+      "Lüscher erstellt filmische UIs mit statischem HTML und CSS. Anwendungen sind z. B. Hacking-Szenen und Fake-Betriebssysteme. Alle Tools sind offlinefähig."
+    ],
+    [
+      "",
+      "metaobjekte.org/peterfischli-steuerung",
+      "Steuerungssysteme für Kunst von Peter Fischli",
+      "Lüscher entwickelte elektronische Steuerungen für 40 Lichtobjekte. Die Arbeiten wurden international ausgestellt. Der Fokus lag auf einfacher Reproduzierbarkeit."
+    ],
+    [
+      "",
+      "audiovisuelles.ch/projekte/erlebniswelt-2019",
+      "Requisitentechnik für interaktive Ausstellung",
+      "Lüscher konzipierte Steuergeräte für eine Dauerausstellung. Elemente reagierten auf das Verhalten der Besuchenden. Elektronik und Sounddesign wurden kombiniert."
+    ]
+  ],  "Digital Props": [
+    [
+      "",
+      "filmtechnik.ch/themen/digitale-requisiten",
+      "Was sind Digital Props im Film?",
+      "Digital Props sind digitale Inhalte auf Bildschirmen, die im Film sichtbar, aber nicht funktional sind. Sie werden für Handys, Tablets, Uhren oder Monitore programmiert. Wichtig ist das Zusammenspiel zwischen Story, Schauspiel und Technik."
+    ],
+    [
+      "",
+      "settechnik.org/hintergrund/interaktive-screens",
+      "Interaktive Bildschirme auf dem Filmset",
+      "Schauspielende interagieren mit Bildschirminhalten, die exakt getimt erscheinen müssen. Die Inhalte laufen oft als lokal gehostete Webapps. Diese müssen offline, flüssig und reaktiv sein."
+    ],
+    [
+      "",
+      "requisitenbau.ch/digitalprops/fehlerquellen",
+      "Häufige Probleme mit digitalen Requisiten",
+      "Unscharfe Inhalte, Latenz oder fehlendes Timing können Takes unbrauchbar machen. Die Abstimmung zwischen Script und UI-Elementen ist entscheidend. Gute Vorbereitung reduziert Nachdrehs."
+    ],
+    [
+      "",
+      "tvproduction.wiki/handys-im-dreh",
+      "Smartphones als Requisite im Film",
+      "Digitale Inhalte auf Handys müssen exakt zu Fingergesten passen. Oft wird mit Screen Capture oder Live-Playback gearbeitet. Ein häufiger Fehler ist das falsche Seitenverhältnis."
+    ],
+    [
+      "",
+      "interfacefilm.ch/blog/ui-styling-fuer-dreh",
+      "UI-Design für Film und TV",
+      "Benutzeroberflächen auf Bildschirmen müssen narrativ verständlich und visuell deutlich sein. Der Look darf nicht von der Szene ablenken. Meist wird mit HTML/CSS oder After Effects gearbeitet."
+    ],
+    [
+      "",
+      "mockinterface.net/toolkits/live-props",
+      "Toolkits für Digital Props am Set",
+      "Live-fähige Tools für Filmrequisiten simulieren SMS, Anrufe oder Web-Inhalte. Sie laufen auf Tablets oder Phones ohne Serververbindung. Timecode-gesteuerte Versionen sind seltener, aber präziser."
+    ],
+    [
+      "",
+      "convo.fakechat/tools/uebersicht",
+      "Chat-Requisiten für Messenger-Szenen",
+      "Simulierte Messenger-Apps zeigen Nachrichten, Emojis oder Töne auf Knopfdruck. Schauspielende müssen passende Bewegungen exakt timen. Tools wie 'Convo' oder 'ChatFX' sind branchenüblich."
+    ],
+    [
+      "",
+      "hackingvisuell.ch/filmtools/fakeos",
+      "Fake-Betriebssysteme für Hacking-Szenen",
+      "Für Filme werden oft eigens entworfene Betriebssysteme simuliert. Diese laufen lokal im Browser oder per USB-Stick. Ziel ist ein glaubwürdiger, aber steuerbarer Screen-Content."
+    ],
+    [
+      "",
+      "filmset-tools.ch/uebersicht/desktopemulator",
+      "Desktop-Simulation für Filmrequisiten",
+      "Fake-Desktops werden verwendet, um Softwareinteraktionen nachzustellen. Sie beinhalten Ordner, Apps, Pop-ups und Systemmeldungen. Timing und UI-Feedback sind zentrale Anforderungen."
+    ],
+    [
+      "",
+      "screenprops.ch/beispiele/tablets",
+      "Tablets mit Fake-Inhalten auf Filmsets",
+      "Oft werden statische Webseiten als Fake-App genutzt. Sie laden lokal und lassen sich per Touch bedienen. Die Inhalte sind meist minimalistisch gehalten."
+    ],
+    [
+      "",
+      "filmhardware.org/blog/smartwatches",
+      "Smartwatches als Digital Props",
+      "Wearables werden zunehmend als Requisiten eingesetzt. Inhalte wie Puls, Maps oder Benachrichtigungen müssen vorab animiert werden. Interaktionen mit Uhren erfordern hohe Präzision."
+    ],
+    [
+      "",
+      "propreview.ch/interview/requisitenentwicklung",
+      "Einblick in die Entwicklung digitaler Filmrequisiten",
+      "Die Arbeit beginnt oft mit Scriptanalyse und UI-Wireframes. Danach folgt der technische Aufbau auf Basis von Webtechnologie. Debugging erfolgt meist direkt am Gerät."
+    ],
+    [
+      "",
+      "postvis.ch/vergleich/live-vs-vfx",
+      "Live-Digitalprops vs. Postproduktion",
+      "Live-Props vermeiden aufwendige VFX-Arbeit in der Postproduktion. Sie ermöglichen natürliches Schauspiel mit echtem Feedback. Dafür ist mehr Vorbereitung und Technik am Set nötig."
+    ],
+    [
+      "",
+      "fakeapps.org/database/propsoftware",
+      "Sammlung von Digital-Prop-Software",
+      "Eine Übersicht von Open Source und kommerziellen Tools für Fake-Screens. Viele basieren auf JavaScript und Vue.js. Einige Tools lassen sich über MQTT oder OSC fernsteuern."
+    ],
+    [
+      "",
+      "propnetzwerk.ch/usecases/krimiserie",
+      "Use Case: Digitale Requisite in Krimiserie",
+      "Ein virtueller Desktop zeigt belastende Dateien und Browser-Historie. Die Inhalte mussten framegenau zur Handlung passen. Die Szenen wurden in Echtzeit mitgefilmt."
+    ],
+    [
+      "",
+      "netzwerkfilm.ch/forum/digitalprops-vorschriften",
+      "Rechtliche Aspekte digitaler Requisiten",
+      "Verwendete UI-Elemente dürfen keine echten Logos oder Markennamen enthalten. Copyrights und UI-Patente sind zu beachten. Viele Produktionen setzen deshalb auf generische Interfaces."
+    ],
+    [
+      "",
+      "videocontrol.ch/artikel/propsteuerung",
+      "Steuerung von Screens über Funk",
+      "Für synchrone Bildschirme wird oft eine Fernsteuerung verwendet. Sie nutzt RF, Bluetooth oder WLAN, um Playback zu kontrollieren. Geringe Latenz ist hier entscheidend."
+    ],
+    [
+      "",
+      "timingsystem.ch/tutorial/fake-anrufe",
+      "Fake-Anrufe auf Filmhandys simulieren",
+      "Klingeltöne, Vibrationen und UI müssen mit Schauspiel synchron sein. Oft kommen vorbereitete Videos oder interaktive Prototypen zum Einsatz. Fehler sind meist in der Tonspur hörbar."
+    ],
+    [
+      "",
+      "filmvisuell.ch/uxdesign/digitalfilm",
+      "UX Design für filmische Interfaces",
+      "Anders als echte UIs sollen filmische Interfaces nur wirken, nicht funktionieren. Sie sind oft überzeichnet, damit das Publikum sie sofort versteht. Farben und Kontraste werden bewusst überhöht."
+    ],
+    [
+      "",
+      "setarchiv.ch/doku/visuelle-requisiten",
+      "Dokumentation: Visuelle Requisiten am Set",
+      "Screens und Displays werden mit rigiden Halterungen oder Greenscreens ersetzt. Bei Digital Props entfällt dieser Schritt. Der Aufwand verschiebt sich in die Vorproduktion."
+    ],
+    [
+      "",
+      "filmtools.ch/showcase/mockbrowser",
+      "Mock-Browser für Online-Recherchen in Filmen",
+      "Fiktive Suchmaschinen, Tabs und Scroll-Verhalten werden inszeniert. Schauspielende scrollen oder klicken live am Set. Der Content ist vollständig lokal geladen."
+    ],
+    [
+      "",
+      "hackingszenen.org/requisiten/shells",
+      "Command Line als Filmrequisite",
+      "Für Hacking-Szenen werden Terminalemulatoren mit Fake-Texten genutzt. Eingaben werden gescriptet und mit Animation getarnt. Die Screens laufen oft auf Raspberry Pi."
+    ],
+    [
+      "",
+      "signalplay.ch/techniken/latenzprobleme",
+      "Latenzprobleme bei digitalen Requisiten",
+      "Verzögerungen bei UI-Reaktionen machen Takes unbrauchbar. Tools müssen auf minimalen Delay optimiert sein. Häufig hilft ein Preload aller Assets am Gerät."
+    ],
+    [
+      "",
+      "telefabi.ch/blog/von-analog-zu-digital",
+      "Von physischen zu digitalen Requisiten",
+      "Digitale Props ersetzen immer häufiger mechanische Lösungen. Sie sparen Platz und ermöglichen mehr Flexibilität. Dafür steigt der technische Aufwand bei der Umsetzung."
+    ],
+    [
+      "",
+      "szenebeispiel.ch/fakescreens/buero",
+      "Beispiel: Fake-Bürosoftware für Businessfilm",
+      "Für einen Werbedreh wurde ein eigenes Mail- und Kalenderinterface entworfen. Die Schauspielerin tippte auf ein Touchdisplay mit Offline-Webseite. Fehlerhafte Animationen mussten nachgedreht werden."
+    ],
+    [
+      "",
+      "screenlogik.ch/blog/realitaetsabgleich",
+      "Wie realistisch müssen digitale Props sein?",
+      "Das Publikum erkennt echte vs. gefakte Interfaces meist unbewusst. Zu viel Realismus kann verwirren, zu wenig wirkt billig. Der Sweet Spot liegt in Funktionalität ohne Tiefe."
+    ],
+    [
+      "",
+      "digitalfilmdesign.ch/tools/ui-engine",
+      "UI-Engines für Filmprops",
+      "Einige Requisiten nutzen spezialisierte UI-Engines mit animierten States. Diese werden über Skripte oder Touchpoints getriggert. Eingesetzt werden z. B. Vue.js, Framer oder Unity."
+    ],
+    [
+      "",
+      "digitaleset.ch/handbuch/livecontent",
+      "Live-Content Playback auf Filmsets",
+      "Playback-Stationen synchronisieren mehrere Screens simultan. Inhalte wie Animationen, Texte oder Videos werden zentral gesteuert. Die Systeme müssen offlinefähig sein."
+    ],
+    [
+      "",
+      "proptechnik.ch/analyse/fehler",
+      "Analyse typischer Prop-Fehler im Film",
+      "Zu schnelle Scrolls, falsche UI-Reaktionen oder nicht synchronisierte Sounds sind häufige Fehler. Diese stören den Erzählfluss. Gute Props fallen nicht auf – und genau das ist die Kunst."
+    ],
+    [
+      "",
+      "storyinterface.ch/editorial/schnittstelle-drehbuch",
+      "Die Schnittstelle zwischen Drehbuch und Interface",
+      "UI-Inhalte müssen dramaturgisch exakt auf die Szene abgestimmt sein. Häufig wird schon im Script eine grobe Interface-Struktur mitgedacht. Gute Digital Props sind oft unauffällig, aber wesentlich."
+    ]
+  ],
   "Leuchtpetarden": [
     [
       "",
